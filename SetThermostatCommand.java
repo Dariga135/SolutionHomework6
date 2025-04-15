62bc6752349023dbbb4955d6fa3c4f7395c774c0
Dariga135 <dariga.dengelbayeva@narxoz.kz>
2025-04-15 20:50:01 +0500

    .git

---
+++ SetThermostatCommand.java
@@ -0,0 +1,15 @@
+class SetThermostatCommand implements Command {
+    private Thermostat thermostat;
+    private int temp;
+
+    public SetThermostatCommand(Thermostat thermostat, int temp) {
+        this.thermostat = thermostat;
+        this.temp = temp;
+    }
+    public void execute() {
+        thermostat.setTemperature(temp);
+    }
+    public void undo() {
+        thermostat.revertTemperature();
+    }
+}
